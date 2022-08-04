31078c2fdc467b2a0dc61602ec3631de58d20191
Zuleika Tesei <zuleikatesei@zuleikas-mbp.lan>
2022-08-04 12:57:48 -0700

    checkAnswer function works but no way to end game

--- game.js
+++ game.js
@@ -2,7 +2,6 @@
 var startButton = document.getElementById('start-btn') 
 var questionEl = document.querySelector('#displayed-question');
 var questionAnswers = document.querySelector('#game-btns');
-var newQuestion 
 var scoreText = document.querySelector('#timer');
 var currentIndex = 0
 var answerEl = document.getElementById('game-btns');
@@ -28,33 +27,35 @@ var questionsArr = [
         title: 'What does the JavaScript debugger statement do?',
         choices: ['It will debug all the errors in the program.', 'It will debug error in the current statement', 
         'It acts a breakpoint in a program', 'All of the above'],
-        answer: 'It acts a breakpoint in a program'
+        answer: 'It acts a breakpoint in a program',
     },
     {
         title: 'How can a datatype be declared to be a constant type?',
         choices: ['var', 'const', 'constant', 'let'],
         answer: 'const',
+    },
+    {
+        title: 'Inside which HTML element do we put the JavaScript?',
+        choices: ['<javascript>', '<main>', '<script>', '<img>'],
+        answer: 'script'
     }
 ];
 
-var currentQuestion = {};
-// var score = 60
-// var questionCounter = 0 
-// var availableQuestions = []
-
 // Setting up the functions 
 function startQuiz() {
+    // confirm game start
     console.log("game start")
+    // hide start screen 
     var startScreen = document.querySelector('.start-screen')
     startScreen.setAttribute('class', 'hide')
+    // display quiz screen
     var quizScreen = document.querySelector('#quiz-screen')
     quizScreen.classList.remove('hide')
-
-    newQuestion = questionsArr[currentIndex]
+    // time starts at 60 seconds 
     score = 60 
     startTime()
+    // display question and answers
     displayQuestion()
-    // getNewQuestion()
 }
 
 startButton.onclick = startQuiz
@@ -73,6 +74,7 @@ function displayQuestion() {
 
     var currentQuestionObj = questionsArr[currentIndex]
     console.log(currentQuestionObj)
+
     var currentQuestion = questionsArr[currentIndex].title
     questionEl.textContent = currentQuestion
     console.log(currentQuestion)   
@@ -82,85 +84,35 @@ function displayQuestion() {
         var choiceButton = document.createElement("button") 
         choiceButton.setAttribute("value", choice)
         choiceButton.setAttribute("class", "btn")
-        choiceButton.textContent = `${i+1}. ${choice}` 
+        choiceButton.textContent = choice
+        // choiceButton.textContent = `${i+1}. ${choice}` 
         answerEl.appendChild(choiceButton)
     }
-
-    // // var currentAnswers = questionsArr[currentIndex].choices 
-    // choiceA.textContent = questionsArr[currentIndex].choices[0]
-    // choiceB.textContent = questionsArr[currentIndex].choices[1]
-    // choiceC.textContent = questionsArr[currentIndex].choices[2]
-    // choiceD.textContent = questionsArr[currentIndex].choices[3]
 }
+
+answerEl.onclick = checkAnswer
 
 function checkAnswer (event) {
-    var userAnswer = event.target
-    var correctAnswer = questionsArr[currentIndex].answer
+    var userAnswer = event.target.textContent
     console.log(userAnswer)
+
+    var correctAnswer = questionsArr[currentIndex].answer
     console.log(correctAnswer)
+
+    // if userAnswer equals correctAnswer
+    if (userAnswer === correctAnswer) {
+        console.log("Good, that's correct!")
+        currentIndex++
+        displayQuestion()
+    } else {
+        // subtract 10 from score
+        score = score - 10 
+        console.log("WRONG ANSWER! -10 points")
+        // next question 
+        currentIndex++
+        displayQuestion()
+    }
 }
-
-// function checkAnswer(event) {
-//     var userAnswer =  event.target.innerText
-//     var correctAnswer = questionsArray[0].answer
-//     console.log(correctAnswer)
-//     console.log(userAnswer)
-//     // check answers 
-//     // if correct answer, get next question 
-//     if (userAnswer === correctAnswer) { 
-//         //go next question
-//         console.log('correct answer!')
-//         getNewQuestion() 
-//     } else { // if incorrect answer, get next question and subtract 10sec from score
-//         // go next question
-//         console.log('wrong answer!')
-//         getNewQuestion() 
-//         // subtract 10 from score 
-//     }
-// }
-
-
-// function getNewQuestion () {
-//     currentIndex++
-//     newQuestion = questionsArr[currentIndex].title
-//     questionEl.textContent = newQuestion
-// }
-
-// for (currentIndex = 0; currentIndex < questionsArr.length; currentIndex++) {
-//     console.log(questionsArr[currentIndex].title);
-// }
-
-// check answer function 
-// if user clicks correct answer where choice = questionsArr[answer] 
-    // then get new question 
-
-    
-    // if user clicks wrong answer where choice /= questionsArr[answer]
-    // subtract 10 from score 
-    // get new question
-
-
-//  function getNewQuestion() {
-//     console.log(availableQuestions.length)
-//     console.log(questionCounter)
-//     console.log(maxQuestions)
-//     // if there are no available questions, save score to local storage and go to end.html 
-//     if(questionCounter > maxQuestions) {
-//         console.log('game end')
-//         // localStorage.setItem('mostRecentScore', score)
-//         // return window.location.assign('end.html')
-//     } else { // else go to next available question 
-//     questionCounter++
-//     // display new question 
-//     currentQuestion = questionsArray[1].question
-//     question.textContent = currentQuestion
-//     // display new answers
-
-//     console.log('getting new question')
-//     console.log(questionCounter)
-//     }
-//     }
-
 
 // startTime();
 
